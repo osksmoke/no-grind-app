a3eb29b36d1d4fdb3a1490ad79e90c5b49bd3257
ChristianTchk <christian.llovera@thebridge.social>
2021-10-25 21:19:57 -0400

    cambio de porcentaje a 0.82

--- src/getPrice.js
+++ src/getPrice.js
@@ -18,7 +18,7 @@ const scraping = async url =>{
         }).get();
         
         const price = rows[0].price
-        const result = (price * 1000) * 0.76
+        const result = (price * 1000) * 0.82
         
         return result.toPrecision(4)
 
