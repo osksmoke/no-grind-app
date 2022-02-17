bd24be707e3ee698b11d591f6e76677f6b48ee70
Zahori Bello <zahoribello@gmail.com>
2022-02-17 17:06:06 -0400

    EDICION MONTO DE JUEGO

--- src/getPrice.js
+++ src/getPrice.js
@@ -18,7 +18,7 @@ const scraping = async url =>{
         }).get();
 
         const price = rows[0].price
-        const result = (price * 1000) * 0.82
+        const result = (price * 1000) * 0.832
         
         return result.toPrecision(4)
 
